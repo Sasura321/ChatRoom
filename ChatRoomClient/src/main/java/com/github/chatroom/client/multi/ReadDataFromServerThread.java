2ee34f35b22b9945f9ee73bab351c3ffe8c18f9c
Sasura321 <2818760446@qq.com>
2019-08-16 16:37:18 +0800

    【修改】发现一个bug，已修改

--- ChatRoomClient/src/main/java/com/github/chatroom/client/multi/ReadDataFromServerThread.java
+++ ChatRoomClient/src/main/java/com/github/chatroom/client/multi/ReadDataFromServerThread.java
@@ -14,18 +14,25 @@ public class ReadDataFromServerThread extends Thread {
 
     @Override
     public void run() {
+        Scanner readFromServer = null;
         try {
-            InputStream clientInput = client.getInputStream();
-            Scanner scanner = new Scanner(clientInput);
+            readFromServer = new Scanner(client.getInputStream());
+            readFromServer.useDelimiter("\n");
+
             while (true) {
-                String message = scanner.nextLine();
-                System.out.println("来自服务器的消息：" + message);
-                if (message.equals("bye")) {
+                if (readFromServer.hasNextLine()) {
+                    String message = readFromServer.nextLine();
+                    System.out.println("来自服务器的消息：" + message);
+                }
+                if (client.isClosed()) {
+                    System.out.println("服务器已经关闭");
                     break;
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            readFromServer.close();
         }
     }
 }
